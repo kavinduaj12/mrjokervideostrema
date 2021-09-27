f306d079f4120fe60e2a7c6206968ee4eaab6b35
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-09-27 13:23:20 +0530

    Update videoplayer.py

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -140,7 +140,7 @@ async def startvideo(client, m: Message):
                     stream_type=StreamType().local_stream,
                 )
                 await m.reply_photo(
-                    photo="https://telegra.ph/file/0faaa780ae861fb780814.png",
+                    photo="https://telegra.ph/file/2939dc9a2c5733fb8de2f.mp4",
                     reply_markup=keyboard,
                     caption=f"💡 **video streaming started!**\n\n🏷 **Name:** {title}\n⏱ **Duration:** `{convert_seconds(duration)} m`\n\n» **join to video chat on the top to watch the video.**")
                 return await msg.delete()
@@ -179,7 +179,7 @@ async def startvideo(client, m: Message):
                 stream_type=StreamType().local_stream,
             )
             await m.reply_photo(
-                photo="https://telegra.ph/file/bca4f149e798092846230.png",
+                photo="https://telegra.ph/file/2939dc9a2c5733fb8de2f.mp4",
                 reply_markup=keyboard,
                 caption=f"💡 **video streaming started !**\n\n» **join to video chat on the top to watch the video.**")
             return await msg.delete()
